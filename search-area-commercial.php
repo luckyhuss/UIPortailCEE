f6e43d590f6bbd1f36eb4dbc027c3c3265c05ac7
rjjpierre <rodney.jeanpierre@gmail.com>
2018-10-12 09:37:41 +0400

    correction et layout calendar

--- search-area-commercial.php
+++ search-area-commercial.php
@@ -1,171 +1,204 @@
 <div class="panel-group">
-<div class="panel panel-default">
-<div class="panel-heading">
-  <h4 class="panel-title">
-    <span class="panel_header_element"><a data-toggle="collapse" href="#collapseSearchBar">Criteres De Recherche</a></span>
-    <span class="panel_header_element"><a data-toggle="collapse" href="#collapseSearchBar"><i class="glyphicon glyphicon-search"></i></a></span>
-  </h4>
-</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<span class="panel_header_element"><a data-toggle="collapse"
+					href="#collapseSearchBar">Criteres De Recherche</a></span> <span
+					class="panel_header_element"><a data-toggle="collapse"
+					href="#collapseSearchBar"><i class="glyphicon glyphicon-search"></i></a></span>
+			</h4>
+		</div>
 
-<div id="collapseSearchBar" class="panel-collapse collapse in">
-<div class="panel-body">
+		<div id="collapseSearchBar" class="panel-collapse collapse in">
+			<div class="panel-body">
 
-<div class="search_area">
+				<div class="search_area">
 
-<form id="formSearchBar" class="form_search_bar form-inline" action="#" method="POST">
+					<form id="formSearchBar" class="form_search_bar form-inline"
+						action="#" method="POST">
 
-<div class="row">
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="NumAction">Num Action</label></span>
-        <span class="form_input"><input type="number" class="form-control" id="numAction" placeholder=""></span>
-        </div>
-    </div>
+						<div class="row">
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="NumAction">Num Action</label></span>
+									<span class="form_input"><input type="number"
+										class="form-control" id="numAction" placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="NumDossier">Reference Externe</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="numDossier" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="NumDossier">Reference
+											Externe</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="numDossier"
+										placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="NomB2C">Nom B2C</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="nomB2C" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="NomB2C">Nom B2C</label></span>
+									<span class="form_input"><input type="text"
+										class="form-control" id="nomB2C" placeholder=""></span>
+								</div>
+							</div>
 
-</div>
+						</div>
 
-<div class="row">
+						<div class="row">
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="StatutPortail">Statut Portail</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="statutPortail" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="StatutPortail">Statut
+											Portail</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="statutPortail"
+										placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="CodePostal">Code Postal</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="codePostal" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="CodePostal">Code Postal</label></span>
+									<span class="form_input"><input type="text"
+										class="form-control" id="codePostal" placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="VilleTravaux">Ville Travaux</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="villeTravaux" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="VilleTravaux">Ville
+											Travaux</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="villeTravaux"
+										placeholder=""></span>
+								</div>
+							</div>
 
 
-</div>
+						</div>
 
-<div class="row">
+						<div class="row">
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="SireneB2B">Sirene B2B</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="sireneB2B" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="SireneB2B">Sirene B2B</label></span>
+									<span class="form_input"><input type="text"
+										class="form-control" id="sireneB2B" placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="RaisonSocialeB2B">Raison Sociale B2B</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="raisonSocialeB2B" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="RaisonSocialeB2B">Raison
+											Sociale B2B</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="raisonSocialeB2B"
+										placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="ChargeAffaireB2B">Charge Affaire B2B</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="chargeAffaireB2B" placeholder=""></span>
-        </div>
-    </div>
-</div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="ChargeAffaireB2B">Charge
+											Affaire B2B</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="chargeAffaireB2B"
+										placeholder=""></span>
+								</div>
+							</div>
+						</div>
 
-<div class="row">
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="SirenePro">Sirene Pro</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="sirenePro" placeholder=""></span>
-        </div>
-    </div>
+						<div class="row">
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="SirenePro">Sirene Pro</label></span>
+									<span class="form_input"><input type="text"
+										class="form-control" id="sirenePro" placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="RaisonSocialePro">Raison Sociale Pro</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="raisonSocialePro" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="RaisonSocialePro">Raison
+											Sociale Pro</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="raisonSocialePro"
+										placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="ChargeAffairePro">Charge Affaire Pro</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="chargeAffairePro" placeholder=""></span>
-        </div>
-    </div>
-</div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="ChargeAffairePro">Charge
+											Affaire Pro</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="chargeAffairePro"
+										placeholder=""></span>
+								</div>
+							</div>
+						</div>
 
-<div class="row">
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="ApporteurAffaireNom">Apporteur Affaire Nom</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="apporteurAffaireNom" placeholder=""></span>
-        </div>
-    </div>
+						<div class="row">
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="ApporteurAffaireNom">Apporteur
+											Affaire Nom</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="apporteurAffaireNom"
+										placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="ApporteurAffaireSiren">Apporteur Affaire Siren</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="apporteurAffaireSiren" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="ApporteurAffaireSiren">Apporteur
+											Affaire Siren</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="apporteurAffaireSiren"
+										placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="PrestataireNom">Prestataire Nom</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="prestataireNom" placeholder=""></span>
-        </div>
-    </div>
-</div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="PrestataireNom">Prestataire
+											Nom</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="prestataireNom"
+										placeholder=""></span>
+								</div>
+							</div>
+						</div>
 
-<div class="row search_last_row">
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="PrestataireSiren">Prestataire Siren</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="prestataireSiren" placeholder=""></span>
-        </div>
-    </div>
+						<div class="row search_last_row">
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="PrestataireSiren">Prestataire
+											Siren</label></span> <span class="form_input"><input
+										type="text" class="form-control" id="prestataireSiren"
+										placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="ReferenceOS">Reference OS</label></span>
-        <span class="form_input"><input type="text" class="form-control" id="referenceOS" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="ReferenceOS">Reference OS</label></span>
+									<span class="form_input"><input type="text"
+										class="form-control" id="referenceOS" placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-4 input_holder">
-        <div class="form-group">
-        <span class="form_label"><label for="VersionOS">Version OS</label></span>
-        <span class="form_input"><input type="number" class="form-control" id="versionOS" placeholder=""></span>
-        </div>
-    </div>
+							<div class="col-md-4 input_holder">
+								<div class="form-group">
+									<span class="form_label"><label for="VersionOS">Version OS</label></span>
+									<span class="form_input"><input type="number"
+										class="form-control" id="versionOS" placeholder=""></span>
+								</div>
+							</div>
 
-    <div class="col-md-12 submit_button_holder">
-        <button type="submit" class="btn btn-primary submit_button">Soumettre</button>
-    </div> 
-</div>
+							<div class="col-md-12 submit_button_holder">
+								<button type="submit" class="btn btn-primary submit_button">Soumettre</button>
+							</div>
+						</div>
 
-</form>
-</div> <!-- End .search_area -->
+					</form>
+				</div>
+				<!-- End .search_area -->
 
-</div>
-</div>
+			</div>
+		</div>
 
-</div>
+	</div>
 </div>
