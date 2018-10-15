023f6f504129c11aecd5cab0f3b28c6ed0dab18b
Noodanam Chedembrun <nchedembrun@astek.mu>
2018-10-15 09:01:59 +0400

    Piloter mon activite - Add filtre

---
+++ filtre.php
@@ -0,0 +1,46 @@
+    
+<div class="panel-group">
+  <div class="panel panel-default">
+  <div class="panel-heading">
+    <h4 class="panel-title">
+      <span class="panel_header_element"><a data-toggle="collapse" href="#collapseFiltre">Filtre</a></span>
+      <span class="panel_header_element"><a data-toggle="collapse" href="#collapseFiltre"><i class="glyphicon glyphicon-sort"></i></a></span>
+    </h4>
+  </div>
+
+  <div id="collapseFiltre" class="panel-collapse collapse in">
+    <div class="panel-body">
+        <div class="col-md-6">
+          <div class="form-group">
+              <span class="form_label"><label for="Filiales">Filiales : </label></span>
+              <span class="form_input">
+              <label class="option_group">
+              <select id='filiales' multiple>
+                <option value='B2B'>B2B</option>
+                <option value='Commercial'>Commercial</option>
+                <option value='Pro'>Pro</option>
+              </select>
+              </label> 
+              </span>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="form-group">
+              <span class="form_label"><label for="Operations">Operations : </label></span>
+              <span class="form_input">
+              <label class="option_group">
+              <select id='operations' multiple>
+                <option value='B2B'>B2B</option>
+                <option value='Commercial'>Commercial</option>
+                <option value='Pro'>Pro</option>
+              </select>
+              </label> 
+              </span>
+          </div>
+        </div>
+
+    </div>
+  </div>
+  </div>
+</div>
