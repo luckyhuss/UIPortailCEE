b6b69ca517cc2f094c5c8eaec235c8c171ddd558
Noodanam Chedembrun <nchedembrun@astek.mu>
2018-10-16 12:42:09 +0400

    Page info cee - contact area

---
+++ contact-area.php
@@ -0,0 +1,31 @@
+<!-- Collapsible wrapper -->
+<div class="panel-group">
+<div class="panel panel-default">
+<div class="panel-heading">
+  <h4 class="panel-title">
+    <span class="panel_header_element"><a data-toggle="collapse" href="#collapseContactArea">Qui contacter?</a></span>
+    <span class="panel_header_element"><a data-toggle="collapse" href="#collapseContactArea"><i class="glyphicon glyphicon-sort"></i></a></span>
+  </h4>
+</div>
+
+<div id="collapseContactArea" class="panel-collapse collapse in">
+  <div class="panel-body">
+
+  <ul class="cards">
+    <li class="col-md-4">
+      <div class="details"><span class="name glyphicon glyphicon-user">BO</span><a class="glyphicon glyphicon-earphone" href="tel:123-456-789">123-456-789</a><a class="glyphicon glyphicon-envelope" href="mailto:emilia.jacobs@email.com">bo@email.com</a></div>
+    </li>
+    <li class="col-md-4">
+      <div class="details"><span class="name glyphicon glyphicon-user">Commercial</span><a class="glyphicon glyphicon-earphone" href="tel:123-456-789">123-456-789</a><a class="glyphicon glyphicon-envelope" href="mailto:tim.pawson@email.com">commercial@email.com</a></div>
+    </li>
+    <li class="col-md-4">
+      <div class="details"><span class="name glyphicon glyphicon-user">Department CEE</span><a class="glyphicon glyphicon-earphone" href="tel:123-456-789">123-456-789</a><a class="glyphicon glyphicon-envelope" href="mailto:amanda.tyler@email.com">dep.cee@email.com				</a></div>
+    </li>
+  </ul>
+
+<!-- Closing collapsible wrapper -->
+</div>
+</div>
+
+</div>
+</div>
