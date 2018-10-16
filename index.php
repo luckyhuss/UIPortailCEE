909629000aca04932f21dd5f7a806c26b5f39221
Noodanam Chedembrun <nchedembrun@astek.mu>
2018-10-16 12:28:57 +0400

    Add new full calendar js

--- index.php
+++ index.php
@@ -55,10 +55,8 @@
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <!-- <script src="assets/js/jquery.min.js"></script> -->
+    <script src="assets/js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed  -->
-    
-    <script src="assets/js/jquery.js"></script>
     
     <script type="text/javascript" src="node_modules/full-calendar/js/moment.min.js"></script>
     <script type="text/javascript" src="node_modules/full-calendar/js/fullcalendar.min.js"></script>
