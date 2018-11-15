f7616c20938f23f47ba045fa9435b267511ec3ae
rjjpierre <rodney.jeanpierre@gmail.com>
2018-11-15 11:48:39 +0400

    font @auth/main for loader

--- filtre.php
+++ filtre.php
@@ -11,28 +11,7 @@
 
 		<div id="collapseFiltre" class="panel-collapse collapse in">
 			<div class="panel-body">
-<!-- 				<ul class="filter-container"> -->
-<!-- 					<li class="filter-item float-item"> -->
-<!-- 						<div class="form-group filtre-option-filiales"> -->
-<!-- 							<span class="form_label"><label for="filiales">Filiales</label></span> -->
-<!-- 							<div id="msFiliale" class="form-control"></div> -->
-<!-- 						</div> -->
-<!-- 					</li> -->
-<!-- 					<li class="filter-item float-item"> -->
-<!-- 						<div class="form-group filtre-option-operations"> -->
-<!-- 							<span class="form_label"><label for="Operations">Operations</label></span> -->
-<!-- 							<div id="msOperation" class="form-control"></div> -->
-<!-- 						</div> -->
-<!-- 					</li> -->
-<!-- 					<li class="filter-item float-item"> -->
-<!-- 						<div class="form-group filtre-option-beneficiaire"> -->
-<!-- 							<span class="form_label"><label for="Beneficiaire">Bénéficiaires -->
-<!-- 									B2B</label></span> -->
-<!-- 							<div id="msBeneficiaire" class="form-control"></div> -->
-<!-- 						</div> -->
-<!-- 					</li> -->
-<!-- 				</ul> -->
-				
+
 				<div class="search_area">
 
 					<form id="formSearchBar" class="form_search_bar form-inline"
@@ -46,55 +25,46 @@
 						<div class="row each-row">
 							<div class="col-md-6 input_holder">
 								<div class="form-group">
-									<span class="form_label"><label for="Filiales">Filiales</label></span> 
-									<div id="msFiliale" class="form-control" ></div>
+									<span class="form_label"><label for="Filiales">Filiales</label></span>
+									<div id="msFiliale" class="form-control"></div>
 								</div>
 							</div>
 
 							<div class="col-md-6 input_holder">
 								<div class="form-group">
 									<span class="form_label"><label for="Operations">Opérations</label></span>
-									<div id="msOperation" class="form-control" ></div>
+									<div id="msOperation" class="form-control"></div>
 								</div>
-                            </div>
-                        </div>    
+							</div>
+						</div>
 						<div class="row each-row">
 							<div class="col-md-6 input_holder">
 								<div class="form-group">
-									<span class="form_label"><label for="Beneficiaires">Bénéficiaires</label></span> 
-											<div id="msBeneficiaire" class="form-control" ></div>
+									<span class="form_label"><label for="Beneficiaires">Bénéficiaires</label></span>
+									<div id="msBeneficiaire" class="form-control"></div>
 								</div>
 							</div>
 
 
 
-<!-- <div class="col-md-6 graph_swap">
-        <div class="form-group">
-            <span class="form_label"><label for="SirenB2B">Change Graph</label></span>
-            <span class="form_input">
-            <select class="form-control select-chart">
-                    <option value="1">En Cours</option>
-                    <option value="2">Deposés</option>
-                    <option value="4">Payé</option>
-                </select>
-            </span>
-        </div>
-    </div> -->
+							<!-- <div class="col-md-6 graph_swap">
+                                <div class="form-group">
+                                    <span class="form_label"><label for="SirenB2B">Change Graph</label></span>
+                                    <span class="form_input">
+                                    <select class="form-control select-chart">
+                                            <option value="1">En Cours</option>
+                                            <option value="2">Deposés</option>
+                                            <option value="4">Payé</option>
+                                        </select>
+                                    </span>
+                                </div>
+                            </div> -->
 
 
 							<div class="col-md-12 submit_button_holder">
 								<button type="submit" class="btn btn-primary submit_button">Rechercher</button>
 							</div>
-							
-							
-	
-							
-							
-                        </div>
-                        
-                       
-                        
-
+						</div>
 
 					</form>
 				</div>
