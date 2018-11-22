77e8e470852af4ba8de239cf1418c836fc4b839b
rjjpierre <rodney.jeanpierre@gmail.com>
2018-11-22 10:03:00 +0400

    gestion utilisateur

---
+++ admin-utilisateur.php
@@ -0,0 +1,21 @@
+<div class="panel-group">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<span class="panel_header_element"><a data-toggle="collapse"
+					href="#infoCee">Gestion Utilisateur</a></span> <span
+					class="panel_header_element"><a data-toggle="collapse"
+					href="#infoCee"><i class="glyphicon glyphicon-user"></i></a></span>
+			</h4>
+		</div>
+
+		<div id="infoCee" class="panel-collapse collapse in">
+			<div class="panel-body">
+			
+			
+			
+			
+			</div>
+		</div>
+	</div>
+</div>
