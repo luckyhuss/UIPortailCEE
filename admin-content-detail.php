2b3db31337665123643f80682de32ff3a3d18705
rjjpierre <rodney.jeanpierre@gmail.com>
2018-11-23 12:14:21 +0400

    include common filiales/secteur

---
+++ admin-content-detail.php
@@ -0,0 +1,110 @@
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h4 class="panel-title">
+			<span class="panel_header_element"><a data-toggle="collapse"
+				href="#collapseContent">Contenu</a></span> <span
+				class="panel_header_element"><a data-toggle="collapse"
+				href="#collapseContent"><i class="glyphicon glyphicon-list"></i></a></span>
+		</h4>
+	</div>
+
+	<div id="collapseContent" class="panel-collapse collapse in">
+		<div class="panel-body">
+
+			<!-- Content of block Content -->
+
+
+			<div class="content-container">
+
+
+					<div class="row">
+
+
+						<div class="col-md-6 input_holder">
+							<div class="form-group">
+								<span class="form_label"><label for="pages">Pages</label></span>
+
+								<span class="form_input"> <select class="form-control">
+										<option value='0'></option>
+										<option value='1'>Page 1</option>
+										<option value='2'>Page 2</option>
+										<option value='3'>Page 3</option>
+										<option value='4'>Page 4</option>
+								</select>
+								</span>
+
+							</div>
+						</div>
+
+
+						<div class="col-md-6 input_holder">
+							<div class="form-group">
+								<span class="form_label"><label for="personas">Personas</label></span>
+
+								<span class="form_input"> <select class="form-control">
+										<option value='0'></option>
+										<option value='1'>Personas 1</option>
+										<option value='2'>Personas 2</option>
+										<option value='3'>Personas 3</option>
+										<option value='4'>Personas 4</option>
+								</select>
+								</span>
+
+							</div>
+						</div>
+						
+
+						<div class="col-md-6 input_holder">
+							<div class="form-group">
+								<span class="form_label"><label for="zones">Blocs</label></span>
+
+								<span class="form_input"> <select class="form-control">
+										<option value='0'></option>
+										<option value='1'>Zones 1</option>
+										<option value='2'>Zones 2</option>
+										<option value='3'>Zones 3</option>
+										<option value='4'>Zones 4</option>
+								</select>
+								</span>
+
+							</div>
+						</div>
+						
+						<div class="col-md-6 input_holder">
+							<div class="form-group">
+								<span class="form_label"><label for="zones">Sous blocs</label></span>
+
+								<span class="form_input"> <select class="form-control">
+										<option value='0'></option>
+										<option value='1'>Zones 1</option>
+										<option value='2'>Zones 2</option>
+										<option value='3'>Zones 3</option>
+										<option value='4'>Zones 4</option>
+								</select>
+								</span>
+
+							</div> 
+						</div>
+
+						
+
+						<div class="col-md-12 input_holder">
+							<div class="form-group">
+								<span class="form_label"><label for="texte">Texte</label></span>
+
+								<span class="form_input" style="width: 100% !important"> <textarea
+										id="summernote-<?=$summernote ?>" class="form-textarea" rows="5"></textarea>
+								</span>
+
+							</div>
+						</div>
+
+					</div>
+		
+			</div>
+
+
+		</div>
+	</div>
+</div>
