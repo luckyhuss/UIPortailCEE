c84bc8d81c0d2a10eadc54b4d5e1f33cf841b6a4
Noodanam Chedembrun <nchedembrun@astek.mu>
2018-11-26 11:46:37 +0400

    Info cee - documents

--- info-cee.php
+++ info-cee.php
@@ -160,94 +160,83 @@
             <div class="panel-body">
 
                 <div class="numberlist">
-                    <ul class="nav nav-pills nav-stacked col-sm-3 col-md-4"
-                        role="tablist">
-                        <li role="presentation" class="empty">
-                            <a class="expand"
-                               href="#tab_a" data-toggle="tab" aria-controls="tab_a" role='tab'>
-                                <h2>
-                                    Convention Mandat
-                                </h2>
-                            </a>
-                        </li>
-                        <li role="presentation" class='empty'>
-                            <a class="expand"
-                               href="#tab_b" data-toggle="tab" aria-controls="tab_b" role='tab'><h2>AH</h2></a>
-                        </li>
-                        <li role="presentation" class='empty'>
-                            <a class="expand"
-                               href="#tab_c" data-toggle="tab" aria-controls="tab_c" role='tab'><h2>ER</h2></a>
-                        </li>
-                        <li role="presentation" class='empty'>
-                            <a class="expand"
-                               href="#tab_d" data-toggle="tab" aria-controls="tab_d" role='tab'><h2>AH</h2></a>
-                        </li>
-                        <li role="presentation" class='empty'>
-                            <a class="expand"
-                               href="#tab_e" data-toggle="tab" aria-controls="tab_e" role='tab'><h2>ER</h2></a>
-                        </li>
-                        <!-- <li role="presentation" class='active empty  visible-xs hidden-xs' ><a href="#null" data-toggle="tab" aria-controls="null" role='tab'>Null</a></li> -->
-                    </ul>
-
-                    <div class="tab-content col-sm-8 col-md-6 New-Fade displayed">
-                        <div class="tab-pane" id="tab_a" role="tabpanel">
-                            <h4>Convention Mandat</h4>
-                            <p>
-                                <i>Information à fournir par la MOA</i><br /><br />
-                                Qu'est ce que c'est ?
-                                <br />
-                                A quoi ça sert ?
-                                <br />
-                                Pourquoi c'est important ?
-                            </p>
-                        </div>
-                        <div class="tab-pane" id="tab_b" role="tabpanel">
-                            <h4>AH</h4>
-                            <p>
-                                <i>Information à fournir par la MOA</i><br /><br />
-                                Qu'est ce que c'est ?
-                                <br />
-                                A quoi ça sert ?
-                                <br />
-                                Pourquoi c'est important ?
-                            </p>
-                        </div>
-                        <div class="tab-pane" id="tab_c" role="tabpanel">
-                            <h4>ER</h4>
-                            <p>
-                                <i>Information à fournir par la MOA</i><br /><br />
-                                Qu'est ce que c'est ?
-                                <br />
-                                A quoi ça sert ?
-                                <br />
-                                Pourquoi c'est important ?
-                            </p>
-                        </div>
-                        <div class="tab-pane" id="tab_d" role="tabpanel">
-                            <h4>ER</h4>
-                            <p>
-                                <i>Information à fournir par la MOA</i><br /><br />
-                                Qu'est ce que c'est ?
-                                <br />
-                                A quoi ça sert ?
-                                <br />
-                                Pourquoi c'est important ?
-                            </p>
-                        </div>
-                        <div class="tab-pane" id="tab_e" role="tabpanel">
-                            <h4>ER</h4>
-                            <p>
-                                <i>Information à fournir par la MOA</i><br /><br />
-                                Qu'est ce que c'est ?
-                                <br />
-                                A quoi ça sert ?
-                                <br />
-                                Pourquoi c'est important ?
-                            </p>
+                    <div id="tab-container" aria-multiselectable="false" class="tabs-view">
+                        <ul class="nav nav-pills nav-stacked col-md-4" id="navlist" role="tablist">
+                            <li role="presentation">
+                            <a class="expand" role="tab" id="one" aria-setsize="3" aria-posinset="1" tabindex="0" href="javascript:void(0);" aria-controls="panel-1" aria-selected="false">Convention mandat</a>
+                            </li>
+                            <li role="presentation">
+                            <a class="expand" role="tab" id="two" aria-setsize="3" aria-posinset="2" tabindex="-1" href="javascript:void(0);" aria-controls="panel-2" aria-selected="false">AH</a>
+                            </li>
+                            <li role="presentation">
+                            <a class="expand" role="tab" id="three" aria-setsize="3" aria-posinset="3" tabindex="-1" href="javascript:void(0);" aria-controls="panel-3" aria-selected="false">ER</a>
+                            </li>
+                                <li role="presentation">
+                            <a class="expand" role="tab" id="four" aria-setsize="3" aria-posinset="4" tabindex="-1" href="javascript:void(0);" aria-controls="panel-4" aria-selected="false">Autre</a>
+                            </li>
+                                <li role="presentation">
+                            <a class="expand" role="tab" id="five" aria-setsize="3" aria-posinset="5" tabindex="-1" href="javascript:void(0);" aria-controls="panel-5" aria-selected="false">Autre</a>
+                            </li>
+                        </ul>
+                        <div id="panels" class="tab-content col-md-6 displayed">
+                            <div role="tabpanel" aria-labelledby="one" class="panel current" tabindex="-1" id="panel-1">
+                                <h4>Convention Mandat</h4>
+                                <p>
+                                    <i>Information à fournir par la MOA</i><br /><br />
+                                    Qu'est ce que c'est ?
+                                    <br />
+                                    A quoi ça sert ?
+                                    <br />
+                                    Pourquoi c'est important ?
+                                </p>
+                            </div>
+                            <div aria-hidden="true" class="panel" role="tabpanel" aria-labelledby="two" tabindex="-1" id="panel-2">
+                                <h4>AH</h4>
+                                <p>
+                                    <i>Information à fournir par la MOA</i><br /><br />
+                                    Qu'est ce que c'est ?
+                                    <br />
+                                    A quoi ça sert ?
+                                    <br />
+                                    Pourquoi c'est important ?
+                                </p>
+                            </div>
+                            <div aria-hidden="true" class="panel" role="tabpanel" aria-labelledby="three" tabindex="-1" id="panel-3">
+                                <h4>ER</h4>
+                                <p>
+                                    <i>Information à fournir par la MOA</i><br /><br />
+                                    Qu'est ce que c'est ?
+                                    <br />
+                                    A quoi ça sert ?
+                                    <br />
+                                    Pourquoi c'est important ?
+                                </p>
+                            </div>
+                            <div aria-hidden="true" class="panel" role="tabpanel" aria-labelledby="four" tabindex="-1" id="panel-4">
+                                <h4>Autre</h4>
+                                <p>
+                                    <i>Information à fournir par la MOA</i><br /><br />
+                                    Qu'est ce que c'est ?
+                                    <br />
+                                    A quoi ça sert ?
+                                    <br />
+                                    Pourquoi c'est important ?
+                                </p>
+                            </div>
+                            <div aria-hidden="true" class="panel" role="tabpanel" aria-labelledby="five" tabindex="-1" id="panel-5">
+                                <h4>Autre</h4>
+                                <p>
+                                    <i>Information à fournir par la MOA</i><br /><br />
+                                    Qu'est ce que c'est ?
+                                    <br />
+                                    A quoi ça sert ?
+                                    <br />
+                                    Pourquoi c'est important ?
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
 
@@ -255,11 +244,177 @@
 </div>
 
 <script>
-$( ".expand" ).click(function() {
-	$(".tab-content").css("display", "block");
-});
+    var $navlist = $('#navlist');
+    var $tabContainer = $('#tab-container');
+    var $panels = $('#panels');
 
-var $menu = $('.tab-content');
+    $navlist.on('keydown', 'li a', function (keyVent) {
+    var arrows = [37, 38, 39, 40];
+    var which = keyVent.which;
+    var target = keyVent.target;
+    if ($.inArray(which, arrows) > -1) {
+        var adjacentTab = findAdjacentTab(target, $navlist, which);
+
+        if (adjacentTab) {
+        keyVent.preventDefault();
+        adjacentTab.focus();
+        // if desired behavior is that when tab receives focus -> make it the active tab:
+        setActiveAndInactive(adjacentTab, $navlist);
+        }
+    } else if (which === 13 || which === 32) { // ENTER |or| SPACE
+        keyVent.preventDefault(); // don't scroll the page around...
+        target.click();
+    } else if (which === 34) { // PAGE DOWN
+        keyVent.preventDefault(); // don't scroll the page
+        var assocPanel = $('#' + this.getAttribute('aria-controls'));
+        if (assocPanel) {
+        assocPanel.focus();
+        }
+    }
+    });
+
+    $(document.body).on('keydown', '.panel', function (e) {
+    if (e.which === 33) { // PAGE UP
+        e.preventDefault(); // don't scroll
+        var activeTab = $navlist.find('li.active a')[0];
+        if (activeTab) {
+        activeTab.focus();
+        }
+    }
+    });
+
+    // click support
+    $navlist.on('click', 'li a', function () {
+    setActiveAndInactive(this, $navlist);
+    });
+
+    function findAdjacentTab(startTab, $list, key) {
+    var dir = (key === 37 || key === 38) ? 'prev' : 'next';
+    var adjacentTab = (dir === 'prev') ?
+                        $(startTab.parentNode).prev()[0] :
+                        $(startTab.parentNode).next()[0];
+
+    if (!adjacentTab) {
+        var allTabs = $list.find('li');
+        if (dir === 'prev') {
+        adjacentTab = allTabs[allTabs.length - 1];
+        } else {
+        adjacentTab = allTabs[0];
+        }
+    }
+
+    return $(adjacentTab).find('a')[0];
+    }
+
+    function setActiveAndInactive(newActive, $list) {
+    $list.find('li').each(function () {
+        var assocPanelID = $(this)
+                            .find('a')
+                            .first()
+                            .attr('aria-controls');
+        var anchor = $(this).find('a')[0];
+
+        if (this !== newActive.parentNode) {
+        $(this).removeClass('active');
+        anchor.tabIndex = -1;
+        anchor.setAttribute('aria-selected', 'false');
+        $('#' + assocPanelID)
+            .removeClass('current')
+            .attr('aria-hidden', 'true');
+        } else {
+        $(this).addClass('active');
+        anchor.tabIndex = 0;
+        anchor.setAttribute('aria-selected', 'true');
+        $('#' + assocPanelID)
+            .addClass('current')
+            .removeAttr('aria-hidden');
+        }
+
+    });
+    }
+
+    // initial configuration based on window's width
+    var isAccordionView = false;
+    var isTabsView = false;
+
+    determineView();
+
+    (function($, sr){
+    var debounce = function (func, threshold, execAsap) {
+        var timeout;
+
+        return function debounced () {
+            var obj = this, args = arguments;
+            function delayed () {
+                if (!execAsap)
+                    func.apply(obj, args);
+                timeout = null;
+            }
+
+            if (timeout)
+                clearTimeout(timeout);
+            else if (execAsap)
+                func.apply(obj, args);
+
+            timeout = setTimeout(delayed, threshold || 100);
+        };
+    };
+    // smartresize
+    jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+    })(jQuery,'smartresize');
+
+
+    // RESIZE EVENT:
+    $(window).smartresize(determineView);
+
+
+    function determineView() {
+    var winWidth = $(window).width();
+
+    if (winWidth <= 991 && !isAccordionView) { // SHOW ACCORDION VIEW
+        // switch to the accordion view
+        $tabContainer
+        .removeClass('tabs-view')
+        .addClass('accordion-view');
+
+        // fix the markup to be more suited for accordions
+        $panels.find('.panel').each(function () {
+        var panelID = this.id;
+        var assocLink = panelID && $('#navlist a[aria-controls="' + panelID + '"]')[0];
+        if (assocLink) {
+            $(assocLink.parentNode).append(this);
+        }
+        });
+
+        isAccordionView = true;
+        isTabsView = false;
+    } else if (winWidth > 991 && !isTabsView) { // SHOW TABS VIEW
+        var wasAccordion = $tabContainer.hasClass('accordion-view');
+        // switch to the tabs view
+        $tabContainer
+        .removeClass('accordion-view')
+        .addClass('tabs-view');
+
+        if (wasAccordion) {
+        $navlist.find('.panel').each(function () {
+            $panels.append(this);
+        });
+        }
+
+        isTabsView = true;
+        isAccordionView = false;
+
+        
+        $( ".expand" ).click(function() {
+        $("#panels").css("display", "block");
+        });
+    }
+    }
+
+
+
+var $menu = $('#panels');
 
 $(document).mouseup(function (e) {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
