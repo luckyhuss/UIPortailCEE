3e359e1abf601b2ea97018a03ae20c93baffc830
rjjpierre <rodney.jeanpierre@gmail.com>
2018-11-28 12:31:54 +0400

    rendering fiche recapitulatif

--- resultatRechercherFiche.php
+++ resultatRechercherFiche.php
@@ -20,8 +20,8 @@
 							style="width: 100%">
 							<thead>
 								<tr>
-                                    <th>SIREN</th>
-                                    <th>Raison sociale</th>
+									<th>SIREN</th>
+									<th>Raison sociale</th>
 									<th>Maison mere</th>
 									<th></th>
 								</tr>
@@ -31,22 +31,28 @@
 									<td>23762328</td>
 									<td>Raison 1</td>
 									<td>123</td>
-									<td class="iconCol"><a href="javascript:void(0)" onclick="displayInfoAdmin();"><i class="glyphicon glyphicon-eye-open"
-											data-toggle="collapse" data-target="#collapseInfoAdmin"></i></a></td>
+									<td class="iconCol"><a href="javascript:void(0)"
+										onclick="displayInfoAdmin();"><i
+											class="glyphicon glyphicon-eye-open" data-toggle="collapse"
+											data-target="#collapseInfoAdmin"></i></a></td>
 								</tr>
 								<tr>
-                                    <td>23762328</td>
+									<td>23762328</td>
 									<td>Raison 2</td>
 									<td>123</td>
-									<td class="iconCol"><a href="javascript:void(0)" onclick="displayInfoAdmin();"><i class="glyphicon glyphicon-eye-open"
-											data-toggle="collapse" data-target="#collapseInfoAdmin"></i></a></td>
+									<td class="iconCol"><a href="javascript:void(0)"
+										onclick="displayInfoAdmin();"><i
+											class="glyphicon glyphicon-eye-open" data-toggle="collapse"
+											data-target="#collapseInfoAdmin"></i></a></td>
 								</tr>
 								<tr>
-                                    <td>23762328</td>
+									<td>23762328</td>
 									<td>Raison 3</td>
 									<td>123</td>
-									<td class="iconCol"><a href="javascript:void(0)" onclick="displayInfoAdmin();"><i class="glyphicon glyphicon-eye-open"
-											data-toggle="collapse" data-target="#collapseInfoAdmin"></i></a></td>
+									<td class="iconCol"><a href="javascript:void(0)"
+										onclick="displayInfoAdmin();"><i
+											class="glyphicon glyphicon-eye-open" data-toggle="collapse"
+											data-target="#collapseInfoAdmin"></i></a></td>
 								</tr>
 							</tbody>
 						</table>
@@ -64,137 +70,225 @@
 <!-- Resultat de la recherche -->
 
 
-<div class="panel-group displayed" id="infoAdmin">
+<div class="panel-group " id="infoAdmin">
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h4 class="panel-title">
 				<span class="panel_header_element"><a data-toggle="collapse"
 					href="#collapseInfoAdmin">Information administrative</a></span> <span
 					class="panel_header_element"><a data-toggle="collapse"
-					href="#collapseInfoAdmin"><i class="glyphicon glyphicon-folder-open"></i></a></span>
+					href="#collapseInfoAdmin"><i
+						class="glyphicon glyphicon-folder-open"></i></a></span>
 			</h4>
 		</div>
 
 		<div id="collapseInfoAdmin" class="panel-collapse collapse in">
 			<div class="panel-body">
 
-			<div class="panel-group" id="infoSociete">
-				<div class="panel panel-default">
-				<div class="panel-body">
-					<span class="tbl-caption col-md-12">Société</span>
-					<div class="header-group col-md-4">
-						<span class="header_label"><label for="raisonSociale">Raison sociale</label></span>
-						<span class="header_output">LOGISTA</span>
+				<fieldset class="fieldset-margin-bottom">
+					<legend>Société</legend>
+
+					<div class="header-group">
+						<span class="header_label"><label for="raisonSociale">Raison
+								sociale</label></span> <span class="header_output">LOGISTA</span>
 					</div>
 
-					<div class="header-group col-md-4">
+					<div class="header-group">
 						<span class="header_label"><label for="Siren">SIREN</label></span>
 						<span class="header_output">394629125</span>
 					</div>
 
-					<div class="header-group col-md-4">
+					<div class="header-group">
 						<span class="header_label"><label for="Filiale">Filiale</label></span>
 						<span class="header_output">Indéfini</span>
 					</div>
 
-					<div class="header-group col-md-4">
+					<div class="header-group">
 						<span class="header_label"><label for="Tel">Téléphone</label></span>
 						<span class="header_output">111222333</span>
 					</div>
 
-					<div class="header-group col-md-4">
+					<div class="header-group">
 						<span class="header_label"><label for="Couriel">Couriel</label></span>
 						<span class="header_output">111222333</span>
 					</div>
 
-					<div class="header-group col-md-4">
+					<div class="header-group">
 						<span class="header_label"><label for="Adresse">Adresse</label></span>
-						<span class="header_output">111222333</span>
+						<span class="header_output">34 Rue de George Gabon, paris 16 eme
+							this is a test this is a test this is a test...</span>
 					</div>
-				</div>
-				</div>
-			</div>
 
-			<div class="panel-group" id="infoSignature">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<span class="tbl-caption col-md-12">Signature Electronique</span>
-						<div class="col-md-12">
-							<span><h6>Dirigeant</h6></span>
-							<div class="header-group col-md-4">
-								<span class="header_label"><label for="nom">Nom</label></span>
-								<span class="header_output">LOGISTA</span>
+
+
+				</fieldset>
+
+
+				<fieldset class="fieldset-margin-bottom">
+					<legend>Signature Electronique</legend>
+
+					<div class="signature-container col-md-10">
+						<div class="signature-section col-md-6">
+							<span class="signature-section-header">Dirigeant</span>
+							<div class="header-group">
+								<span class="header_label"><label for="nom">Nom</label></span> <span
+									class="header_output">LOGISTA</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="prenom">Prénom</label></span>
 								<span class="header_output">394629125</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="Tel">Téléphone</label></span>
 								<span class="header_output">111222333</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="mail">Mail</label></span>
 								<span class="header_output">111222333</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="statut">Statut</label></span>
 								<span class="header_output">Indéfini</span>
 							</div>
 						</div>
 
-						<div class="col-md-12">
-						<span><h6>Responsable CEE</h6></span>
-							<div class="header-group col-md-4">
-								<span class="header_label"><label for="nom">Nom</label></span>
-								<span class="header_output">LOGISTA</span>
+						<div class="signature-section col-md-6">
+							<span class="signature-section-header">Responsable CEE</span>
+							<div class="header-group">
+								<span class="header_label"><label for="nom">Nom</label></span> <span
+									class="header_output">LOGISTA</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="prenom">Prénom</label></span>
 								<span class="header_output">394629125</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="Tel">Téléphone</label></span>
 								<span class="header_output">111222333</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="mail">Mail</label></span>
 								<span class="header_output">111222333</span>
 							</div>
 
-							<div class="header-group col-md-4">
+							<div class="header-group">
 								<span class="header_label"><label for="statut">Statut</label></span>
 								<span class="header_output">Indéfini</span>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
 
-			<div class="panel-group" id="infoConvention">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<span class="tbl-caption col-md-12">Convention</span>
-						<div class="header-group col-md-6">
-							<span class="header_label"><label for="Convention">Convention </label></span>
-							<span class="header_output">Signee le xxx</span>
-						</div>
+				</fieldset>
 
-						<div class="header-group col-md-6">
-							<span class="header_label"><label for="Avenant">Avenant</label></span>
-							<span class="header_output">Signee le xxx</span>
+				<fieldset>
+					<legend>Convention</legend>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Convention">Convention </label></span>
+						<span class="header_output">Signee le xxx</span>
+					</div>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Avenant">Avenant</label></span>
+						<span class="header_output">Signee le xxx</span>
+					</div>
+				</fieldset>
+
+
+
+
+
+				<!-- <div class="panel-group" id="infoSignature">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<span class="tbl-caption col-md-12">Signature Electronique</span>
+							<div class="col-md-12">
+								<span><h6>Dirigeant</h6></span>
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="nom">Nom</label></span>
+									<span class="header_output">LOGISTA</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="prenom">Prénom</label></span>
+									<span class="header_output">394629125</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="Tel">Téléphone</label></span>
+									<span class="header_output">111222333</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="mail">Mail</label></span>
+									<span class="header_output">111222333</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="statut">Statut</label></span>
+									<span class="header_output">Indéfini</span>
+								</div>
+							</div>
+
+							<div class="col-md-12">
+								<span><h6>Responsable CEE</h6></span>
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="nom">Nom</label></span>
+									<span class="header_output">LOGISTA</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="prenom">Prénom</label></span>
+									<span class="header_output">394629125</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="Tel">Téléphone</label></span>
+									<span class="header_output">111222333</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="mail">Mail</label></span>
+									<span class="header_output">111222333</span>
+								</div>
+
+								<div class="header-group col-md-4">
+									<span class="header_label"><label for="statut">Statut</label></span>
+									<span class="header_output">Indéfini</span>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
+
+				<!-- <div class="panel-group" id="infoConvention">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<span class="tbl-caption col-md-12">Convention</span>
+							<div class="header-group col-md-6">
+								<span class="header_label"><label for="Convention">Convention </label></span>
+								<span class="header_output">Signee le xxx</span>
+							</div>
+
+							<div class="header-group col-md-6">
+								<span class="header_label"><label for="Avenant">Avenant</label></span>
+								<span class="header_output">Signee le xxx</span>
+							</div>
+						</div>
+					</div>
+				</div> -->
+
+
 			</div>
-			</div>
+
+
 		</div>
 	</div>
 </div>
@@ -207,31 +301,35 @@
 			<h4 class="panel-title">
 				<span class="panel_header_element"><a data-toggle="collapse"
 					href="#collapseInfoActivite">Information activité</a></span>
-					 <!-- <span class="panel_header_element"><a data-toggle="collapse" href="#collapseInfoActivite"><i class="glyphicon glyphicon-folder-open"></i></a></span> -->
+				<!-- <span class="panel_header_element"><a data-toggle="collapse" href="#collapseInfoActivite"><i class="glyphicon glyphicon-folder-open"></i></a></span> -->
 			</h4>
 		</div>
 
 		<div id="collapseInfoActivite" class="panel-collapse collapse in">
 			<div class="panel-body">
-					<div class="header-group col-md-6">
-						<span class="header_label col-md-6"><label for="docEnCours">Total dossiers en cours </label></span>
-						<span class="header_output col-md-6">2124 € kWhc</span>
-					</div>
+				<div class="header-group col-md-6">
+					<span class="header_label col-md-6"><label for="docEnCours">Total
+							dossiers en cours </label></span> <span
+						class="header_output col-md-6">2124 € kWhc</span>
+				</div>
 
-					<div class="header-group col-md-6">
-						<span class="header_label col-md-6"><label for="docDeposes">Total dossiers deposés</label></span>
-						<span class="header_output col-md-6">153 € kWhc</span>
-					</div>
+				<div class="header-group col-md-6">
+					<span class="header_label col-md-6"><label for="docDeposes">Total
+							dossiers deposés</label></span> <span
+						class="header_output col-md-6">153 € kWhc</span>
+				</div>
 
-					<div class="header-group col-md-6">
-						<span class="header_label col-md-6"><label for="docPayes">Total dossiers payés (BEN) </label></span>
-						<span class="header_output col-md-6">2124 € kWhc</span>
-					</div>
+				<div class="header-group col-md-6">
+					<span class="header_label col-md-6"><label for="docPayes">Total
+							dossiers payés (BEN) </label></span> <span
+						class="header_output col-md-6">2124 € kWhc</span>
+				</div>
 
-					<div class="header-group col-md-6">
-						<span class="header_label col-md-6"><label for="docPayes">Total dossiers payés (PRO)</label></span>
-						<span class="header_output col-md-6">153 € kWhc</span>
-					</div>
+				<div class="header-group col-md-6">
+					<span class="header_label col-md-6"><label for="docPayes">Total
+							dossiers payés (PRO)</label></span> <span
+						class="header_output col-md-6">153 € kWhc</span>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -244,51 +342,51 @@
 		<div class="panel-heading">
 			<h4 class="panel-title">
 				<span class="panel_header_element"><a data-toggle="collapse"
-					href="#collapseInfoOpe">Top 10 opérations </a></span> 
-					<!-- <span class="panel_header_element"><a data-toggle="collapse" href="#collapseInfoOpe"><i class="glyphicon glyphicon-folder-open"></i></a></span> -->
+					href="#collapseInfoOpe">Top 10 opérations </a></span>
+				<!-- <span class="panel_header_element"><a data-toggle="collapse" href="#collapseInfoOpe"><i class="glyphicon glyphicon-folder-open"></i></a></span> -->
 			</h4>
 		</div>
 
 		<div id="collapseInfoOpe" class="panel-collapse collapse in">
 			<div class="panel-body">
-					<table id="operationsFiche"
-						class="table table-striped table-bordered nowrap"
-						style="width: 100%">
-						<thead>
-							<tr>
-								<th>Réference opération standardisée</th>
-								<th>mWhc classique</th>
-								<th>mWhc précartité</th>
-								<th>Statut</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>Dossier 1</td>
-								<td class="numcol">0,000</td>
-								<td class="numcol">0,000</td>
-								<td>En cours</td>
-							</tr>
-							<tr>
-								<td>Dossier 2</td>
-								<td class="numcol">0,000</td>
-								<td class="numcol">0,000</td>
-								<td>En cours</td>
-							</tr>
-							<tr>
-								<td>Dossier 3</td>
-								<td class="numcol">0,000</td>
-								<td class="numcol">0,000</td>
-								<td>En cours</td>
-							</tr>
-						</tbody>
-					</table>
-                    <!-- Top 10 operations -->
-                    
-				</div>
-				<!-- Closing collapsible wrapper -->
+				<table id="operationsFiche"
+					class="table table-striped table-bordered nowrap"
+					style="width: 100%">
+					<thead>
+						<tr>
+							<th>Réference opération standardisée</th>
+							<th>mWhc classique</th>
+							<th>mWhc précartité</th>
+							<th>Statut</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Dossier 1</td>
+							<td class="numcol">0,000</td>
+							<td class="numcol">0,000</td>
+							<td>En cours</td>
+						</tr>
+						<tr>
+							<td>Dossier 2</td>
+							<td class="numcol">0,000</td>
+							<td class="numcol">0,000</td>
+							<td>En cours</td>
+						</tr>
+						<tr>
+							<td>Dossier 3</td>
+							<td class="numcol">0,000</td>
+							<td class="numcol">0,000</td>
+							<td>En cours</td>
+						</tr>
+					</tbody>
+				</table>
+				<!-- Top 10 operations -->
+
 			</div>
+			<!-- Closing collapsible wrapper -->
 		</div>
+	</div>
 </div>
 
 
