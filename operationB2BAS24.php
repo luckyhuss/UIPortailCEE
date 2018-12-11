ca56fb0195c57d2398867eefcb78eb26adbaca6d
rjjpierre <rodney.jeanpierre@gmail.com>
2018-12-11 10:47:27 +0400

    secteur/benef@simuler

--- operationB2BAS24.php
+++ operationB2BAS24.php
@@ -25,8 +25,8 @@
 
 								<div class="header-group col-md-12">
 									<span class="form_label"><label for="Critere">Secteur</label></span>
-									<span class="header_output"><select id="selectSecteur_OP0" 
-										name="secteur_OP0" class="form-control secteur">
+									<span class="header_output"><select id="selectSecteur_OP0"
+										name="selectSecteur_OP0" class="form-control secteur">
 											<option value="0"></option>
 											<option value="1">#1 - Industrie</option>
 											<option value="2">#2 - bâtiment résidentiel (BAR)</option>
@@ -35,10 +35,12 @@
 											<option value="5">#5 - Secteur 5</option>
 									</select></span>
 								</div>
-								<div class="header-group col-md-12">
-									<span class="form_label"><label for="Critere">Type de bénéficiaire</label></span>
-									<span class="header_output"><select id="beneficiaire_OP0"
-										name="beneficiaire_OP0" class="form-control beneficiaire">
+								<div class="header-group col-md-12 displayed"
+									id="sectionBenef_OP0">
+									<span class="form_label"><label for="Critere">Type de
+											bénéficiaire</label></span> <span class="header_output"><select
+										id="beneficiaire_OP0" name="beneficiaire_OP0"
+										class="form-control beneficiaire">
 											<option value="0"></option>
 											<option value="1">#1 - un logement</option>
 											<option value="2">#2 - plusieurs logements</option>
@@ -59,7 +61,7 @@
 								<div class="header-group">
 									<span class="form_label"><label for="Critere">Opération</label></span>
 									<span class="header_output"><select id="selectOperation_OP0"
-										name="secteur_OP0" class="form-control">
+										name="selectOperation_OP0" class="form-control">
 											<option value="0"></option>
 											<option value="1">#1 - Operation 1</option>
 											<option value="2">#2 - Operation 2</option>
@@ -83,56 +85,58 @@
 								<div class="col-sm-6 col-md-6 input_holder">
 									<div class="header-group">
 										<span class="form_label"><label for="Critere1">Critère 1</label></span>
-										<span class="header_output"><input type="text" class="form-control" id="critere1" placeholder=""></span>
+										<span class="header_output"><input type="text"
+											class="form-control" id="critere1" placeholder=""></span>
 									</div>
 								</div>
 
 								<div class="col-sm-6 col-md-6 input_holder">
 									<div class="header-group">
 										<span class="form_label"><label for="Critere2">Critère 2</label></span>
-										<span class="header_output"><input type="text" class="form-control" id="critere2" placeholder=""></span>
+										<span class="header_output"><input type="text"
+											class="form-control" id="critere2" placeholder=""></span>
 									</div>
 								</div>
 
 								<div class="col-sm-6 col-md-6 input_holder">
 									<div class="header-group">
-									<span class="form_label"><label for="Critere3">Critère 3</label></span>
-									<span class="header_output">											
-										<select id="critere3" name="critere3" class="form-control">
+										<span class="form_label"><label for="Critere3">Critère 3</label></span>
+										<span class="header_output"> <select id="critere3"
+											name="critere3" class="form-control">
 												<option value="1">H1</option>
 												<option value="2">H2</option>
 												<option value="3">H3</option>
 										</select>
-									</span>
+										</span>
 									</div>
 								</div>
 
 								<div class="col-sm-6 col-md-6 input_holder">
 									<div class="header-group">
-									<span class="form_label"><label for="Critere4">Critère 4</label></span>
-									<span class="header_output">											
-										<select id="critere4" name="critere4" class="form-control">
+										<span class="form_label"><label for="Critere4">Critère 4</label></span>
+										<span class="header_output"> <select id="critere4"
+											name="critere4" class="form-control">
 												<option value="1">H1</option>
 												<option value="2">H2</option>
 												<option value="3">H3</option>
 										</select>
-									</span>
+										</span>
 									</div>
 								</div>
 
 								<div class="col-sm-6 col-md-6 input_holder">
 									<div class="form-group">
 										<span class="form_label"><label for="Critere5">Critère 5</label></span>
-										<span class="header_output"><input type="checkbox" id="Critere5"
-											placeholder=""></span>
+										<span class="header_output"><input type="checkbox"
+											id="Critere5" placeholder=""></span>
 									</div>
 								</div>
-								
+
 								<div class="col-sm-6 col-md-6 input_holder">
 									<div class="form-group">
 										<span class="form_label"><label for="Critere6">Critère 6</label></span>
-										<span class="header_output"><input type="checkbox" id="Critere6"
-											placeholder=""></span>
+										<span class="header_output"><input type="checkbox"
+											id="Critere6" placeholder=""></span>
 									</div>
 								</div>
 
@@ -160,7 +164,8 @@
 							<div class="panel panel-default">
 								<div class="panel-body">
 									<div class="stat__data col-md-8">
-										<span class="header_label"><label for="RemunerationRefEstimée">Rémunération référence/estimée</label></span>
+										<span class="header_label"><label for="RemunerationRefEstimée">Rémunération
+												référence/estimée</label></span>
 										<p class="header_output">10 €/MWh</p>
 									</div>
 									<div class="stat_icon_wrapper col-md-4">
@@ -240,7 +245,8 @@
 			<div class="panel-body">
 				<div class="resultatCumule">
 					<div class="stat__data col-md-8">
-						<span class="header_label"><label for="ResultatCumule">Résultat cumulé</label></span>
+						<span class="header_label"><label for="ResultatCumule">Résultat
+								cumulé</label></span>
 						<div class="header-group">
 							<span class="header_label"><label for="ResultatCumulerCL">Classique</label></span>
 							<span class="header_output">10 kWhc</span> <span
@@ -301,7 +307,8 @@
 </div>
 
 <script>
-
+//.find("input, input[type='checkbox']")
+//=======
     var regex = /^(.*)(\d)+$/i;
     var cloneIndex = $(".clonedOP").length;
     if ($(".clonedOP").length == 1) {
@@ -314,10 +321,11 @@
         $(this).parents(".clonedOP").clone(true, true)
             .appendTo(".tbodyClone")
             .attr("id", "clonedOP" + (cloneIndex+1))
-            .find("input, input[type='checkbox']")
+            .attr("name", "clonedOP" + (cloneIndex+1))
+            .find("*")
             .each(function () {
-				$(this).val('');
-				$(this).prop("checked", false);
+				//$(this).val('');
+				//$(this).prop("checked", false);
                 var id = this.id || "";
                 var match = id.match(regex) || [];
                 if (match.length == 3) {
@@ -366,49 +374,70 @@
 
 
   </script>
-  
-  <script>
 
-//$(document).ready(function(){
+<script>
+
 	
-
-  
-//	 $('.beneficiaire').click(function () {
-//	 var benef =  $('.beneficiaire').val();
-//	 alert(benef);
-//	})
-//	});
-
-//$(document).ready(function() {
- 
-//	 $( "input[name='optradio']" ).click(function(e) {
-//		 	$('.choix-partenaire').removeClass('displayed');
-//	   		var checkedValue = $("input[name='optradio']:checked").val();
-//	   		//alert('checked ' + checkedValue);
-//	   		if(checkedValue == 'partenaire') {
-//		   		$('.choix-partenaire').addClass('displayed');
-//	   		}
-
-//	   		$('.choix-container').removeClass('displayed');
-//	 });
-
-//	 });
-
-
-
-
-// $(".secteur option:selected").each(function () {
-//     //alert($(this).text());
-//     console.log($(this).text()); 
-// });
 $('select[id^="selectSecteur"]').change(function () {
-	$('select[id^="selectSecteur"]').each(function () {
-		
-	    console.log(this.id);
-		});
-	});
+
+	var isBar = isAnyBarSelected();
+
 	
 
+	var idClicked = $(this).attr('id');
+	console.log('isBar ' + isBar + ' idClicked=> ' + idClicked);
+
+    if(isBar < 2) {
+    	$('select[id^="selectSecteur"]').each(function () {
+    		var valueChecked = $('#' + this.id).val();
+    		var str = this.id;
+    		var idSelected = str.split("_")[1];
+    		if(valueChecked == '2') {
+    			
+    			console.log('id selected BAR ' + this.id + ', => Id= ' + idSelected);
+    			$('#sectionBenef_' + idSelected).removeClass('displayed');
+
+    			
+    		}
+    		else {
+//     			if(valueChecked == '2' && isBar > 1) {
+//     				//clear secteur field and add message
+//     				console.log('id to reset ' + this.id);
+//     				$('#' + this.id).prop("selectedIndex", 0);
+//     			}
+
+    // $('#' + this.id).on('click', function(){
+//         var idclicked = $(this).attr('id');
+//         console.log('idclicked ' + idclicked);
+    // });
+    	    	console.log(this.id);
+    	    	$('#sectionBenef_' + idSelected).addClass('displayed');
+    		}
+    		
+    	});
+
+    }
+    else{
+    	$('#' + idClicked).prop("selectedIndex", 0);
+    }
+	
+	
+	
+});
+	
+
+function isAnyBarSelected() {
+	var isBar = 0;
+	$('select[id^="selectSecteur"]').each(function () {
+		var valueChecked = $('#' + this.id).val();
+		
+		if(valueChecked == '2') {
+			isBar++;
+		}
+		
+	});
+
+	return isBar;
+}
 
   </script>
-    
