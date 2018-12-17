b5558706aaa3ddd1321413133f6935b831a2523c
Noodanam Chedembrun <nchedembrun@astek.mu>
2018-12-17 12:29:29 +0400

    Page historiques

--- simulationHistoriques.php
+++ simulationHistoriques.php
@@ -10,8 +10,8 @@
 <div id="collapseHistoSim" class="panel-collapse collapse in">
         <div class="panel-body">
             <div class="input_holder">
-                <span class="col-xs-10 col-sm-8 col-md-6  form_input"><input type="text" class="form-control searchTerm" id="searchSim" placeholder="Search"></span>
-                <span class="submit_button_holder"><button type="submit" class="btn btn-primary submit_button" name="btn-serach"><i class="glyphicon glyphicon-search"></i></button></span>
+                <span class="col-xs-10 col-sm-8 col-md-6  form_input"><input type="text" class="form-control searchTerm" id="searchSim" placeholder="Rechercher par le nom ou la date du brouillon"></span>
+                <span class="submit_button_holder"><button type="submit" class="btn btn-primary submit_button" name="btn-search"><i class="glyphicon glyphicon-search"></i></button></span>
             </div>
 
             <span class="header_label"><label for="simEnregistree">Dernières simulations enregistrée </label></span>
@@ -21,10 +21,9 @@
                         <thead>
                         <tr>
                             <th>Nom</th>
-                            <th>Ref</th>
-                            <th>Fiche</th>
-                            <th>Client</th>
-                            <th>kWhc</th>
+                            <th>Référence</th>
+                            <th>Date création</th>
+                            <th>MWhc</th>
                             <th>€</th>
                             <th></th>
                         </tr>
@@ -33,8 +32,7 @@
                         <tr>
                             <td>4543451</td>
                             <td>14854</td>
-                            <td>Fiche 1</td>
-                            <td>Client 1</td>
+                            <td>2018/02/05</td>
                             <td class="numcol">645</td>
                             <td class="numcol">545</td>
                             <td class="iconCol"><a href="javascript:void(0)" onclick="displayDetailSimulation();"><i class="glyphicon glyphicon-eye-open"
@@ -43,8 +41,7 @@
                         <tr>
                             <td>4543452</td>
                             <td>14854</td>
-                            <td>Fiche 2</td>
-                            <td>Client 2</td>
+                            <td>2018/02/05</td>
                             <td class="numcol">645</td>
                             <td class="numcol">545</td>
                             <td class="iconCol"><a href="javascript:void(0)" onclick="displayDetailSimulation();"><i class="glyphicon glyphicon-eye-open"
@@ -53,6 +50,9 @@
                         </tbody>
                         </table>
 
+                        <div class="col-md-12 submit_button_holder">
+                            <a href="#" class="btn btn-primary submit_button" role="button">Voir plus</a>
+                        </div> 
                     </div>
                 </div>
         </div>
@@ -64,7 +64,7 @@
     <div class="panel panel-default">
         <div class="panel-heading">
             <h4 class="panel-title">
-                <span class="panel_header_element"><a data-toggle="collapse" href="#collapseResultatRecherche">Resultat</a></span>
+                <span class="panel_header_element"><a data-toggle="collapse" href="#collapseResultatRecherche">Résultat</a></span>
             </h4>
         </div>
 
@@ -76,10 +76,9 @@
             <thead>
             <tr>
                 <th>Nom</th>
-                <th>Ref</th>
-                <th>Fiche</th>
-                <th>Client</th>
-                <th>kWhc</th>
+                <th>Référence</th>
+                <th>Date création</th>
+                <th>MWhc</th>
                 <th>€</th>
                 <th></th>
             </tr>
@@ -88,8 +87,7 @@
             <tr>
                 <td>4543451</td>
                 <td>14854</td>
-                <td>Fiche 1</td>
-                <td>Client 1</td>
+                <td>2018/02/05</td>
                 <td class="numcol">645</td>
                 <td class="numcol">545</td>
                 <td class="iconCol"><a href="javascript:void(0)" onclick="displayDetailSimulation();"><i class="glyphicon glyphicon-eye-open"
@@ -98,8 +96,7 @@
             <tr>
                 <td>4543452</td>
                 <td>14854</td>
-                <td>Fiche 2</td>
-                <td>Client 2</td>
+                <td>2018/02/05</td>
                 <td class="numcol">645</td>
                 <td class="numcol">545</td>
                 <td class="iconCol"><a href="javascript:void(0)" onclick="displayDetailSimulation();"><i class="glyphicon glyphicon-eye-open"
@@ -124,11 +121,42 @@
 
         <div id="collapseDetailSimulation" class="panel-collapse collapse">
             <div class="panel-body">
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <!-- <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span> -->
                 
+                    <div class="header-group">
+						<span class="header_label"><label for="raisonSociale">Nom du brouillon</label></span>
+                        <span class="header_output">Brouillon 1</span>
+					</div>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Siren">Référence</label></span>
+						<span class="header_output">394629125</span>
+					</div>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Filiale">Date de création</label></span>
+						<span class="header_output">2018/03/06</span>
+					</div>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Tel">Nombre d'opération</label></span>
+						<span class="header_output">11</span>
+					</div>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Couriel">Secteur</label></span>
+						<span class="header_output">test</span>
+					</div>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Adresse">Type</label></span>
+						<span class="header_output">test</span>
+					</div>
+
+
                 <div class="col-md-12 submit_button_holder">
-                    <a href="#" data-toggle="modal" data-target="#Modal2" class="btn btn-primary submit_button" role="button">Reprendre</a>
-                    <a href="#" data-toggle="modal" data-target="#supprimerSimModal" class="btn btn-primary submit_button" role="button">Supprimer</a>
+                    <a href="#" class="btn btn-primary submit_button" role="button">Reprendre</a>
+                    <a href="#" data-toggle="modal" data-target="#supprimerSimModal" id="btn_supprimer" class="btn btn-primary submit_button" role="button">Supprimer</a>
                 </div>
             </div>
         </div>
@@ -138,48 +166,7 @@
 <div class="col-md-2 submit_button_holder btn-retour">
     <a class="btn btn-primary submit_button">Retour</a>
 </div>
-<!-- <div class="panel-group">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h4 class="panel-title">
-                <span class="panel_header_element"><a data-toggle="collapse" href="#collapseDetailSim">Detail Simulation</a></span>
-            </h4>
-        </div>
 
-        <div id="collapseDetailSim">
-        <div class="panel-body">
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-        
-            <div class="col-md-12 submit_button_holder">
-                <button type="submit" class="btn btn-primary submit_button">Reprendre</button>
-                <button type="submit" class="btn btn-primary submit_button">Supprimer</button>
-            </div>
-        </div>
-        </div>
-
-    </div>
-</div> -->
-
-<!-- modal detail simulation -->
-<!-- <div class="modal fade" id="Modal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog liste_critere" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="btn btn-default" data-dismiss="modal">x</button>
-            <h4 class="modal-title" id="myModalLabel">Demande</h4>
-        </div>
-
-        <div class="modal-body">
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-        </div>
-
-        <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Reprendre</button>
-            <button type="button" class="btn btn-default" data-dismiss="modal">Supprimer</button>
-        </div>
-        </div>
-    </div>
-</div> -->
 
 <!-- modal surrpimer simulation -->
 <div class="modal fade popin-donnee" id="supprimerSimModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -191,8 +178,32 @@
         </div>
 
         <div class="modal-body">
-            <span id="modal_body">Dossier perdu contre: 
-                prix:
+            <span id="modal_body">
+                <!-- <div class="header-group">
+						<span class="header_label"><label for="DossierPerdu">Dossier perdu contre</label></span>
+						<span class="header_output">Indefini</span>
+					</div>
+
+					<div class="header-group">
+						<span class="header_label"><label for="Adresse">Type</label></span>
+						<span class="header_output">Indefini</span>
+                    </div>  -->
+                    
+                    <div class="col-sm-3 col-md-3 input_holder">
+                        <div class="form-group">
+                            <span class="form_input"><input type="radio" name="optDosPerdu"
+                                value="dosPerdu"></span> <span class="form_label"><label
+                                for="dosPerdu">Dossier perdu</label></span>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-md-3 input_holder">
+                        <div class="form-group">
+                            <span class="form_input"><input type="radio" name="optDosAban"
+                                value="dosAbandonne"></span> <span class="form_label"><label
+                                for="dosAbandonne">Dossier abandonné</label></span>
+                        </div>
+                    </div>
             </span>
 
             <div id="supprimerSimModalMsg" class="modal-msg displayed">
@@ -201,8 +212,8 @@
         </div>
 
         <div class="modal-footer">
-            <span><button type="button" class="btn btn-default" id="modal-btn-non">non</button></span>
-            <span><button type="button" class="btn btn-default" id="modal-btn-oui">oui</button></span>
+            <!-- <span><button type="button" class="btn btn-default" id="modal-btn-non">non</button></span> -->
+            <span><button type="button" class="btn btn-default" id="modal-btn-oui">ok</button></span>
         </div>
         </div>
     </div>
@@ -223,9 +234,9 @@ $( "#supprimerSimModal .modal-footer #modal-btn-oui" ).click(function() {
     $("#supprimerSimModal #modal_body, #supprimerSimModal .modal-footer").css("display", "none");
 });
 
-$( "#supprimerSimModal .modal-footer #modal-btn-non" ).click(function() {
-    $('#supprimerSimModal').modal('hide'); 
-});
+// $( "#supprimerSimModal .modal-footer #modal-btn-non" ).click(function() {
+//     $('#supprimerSimModal').modal('hide'); 
+// });
 
 $("#supprimerSimModal").on("hidden.bs.modal", function(){
     $("#supprimerSimModal #modal_body, #supprimerSimModal .modal-footer").css("display", "block");
