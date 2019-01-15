bd36dcf68d340b36e00aa8598b7f063d8b63c72e
Noodanam Chedembrun <nchedembrun@astek.mu>
2019-01-15 12:02:04 +0400

    Resultat cumulé pour apporteur d'affaires

--- resultat-cumule.php
+++ resultat-cumule.php
@@ -17,14 +17,14 @@
 					</div>
 					<div class="repartition-section col-md-12">
 						<span class="repartition-section-header">Calcul CUMAC</span>
-						<div class="col-sm-6 col-md-6 input_holder">
+						<div class="col-sm-6 col-md-4 input_holder">
 							<div class="header-group">
 								<span class="header_label"><label for="ResultatCumulerCL">Classique</label></span>
 								<span class="header_output" id="resultatCumuleCL_cumac">10 kWhc</span> 
 							</div>
 						</div>
 
-						<div class="col-sm-6 col-md-6 input_holder">
+						<div class="col-sm-6 col-md-4 input_holder">
 							<div class="header-group">
 								<span class="header_label"><label for="ResultatCumulerPR">Précarité</label></span>
 								<span class="header_output" id="resultatCumulePR_cumac">10 kWhc</span>
@@ -33,7 +33,7 @@
 					</div>
 
 
-					<div class="repartition-section col-md-6">
+					<div class="repartition-section col-md-4">
 						<span class="repartition-section-header">Calcul AIDE Beneficiare</span>
 						<div class="col-sm-6 col-md-12 input_holder">
 							<div class="header-group">
@@ -50,7 +50,7 @@
 						</div>
 					</div>
 
-					<div class="repartition-section col-md-6">
+					<div class="repartition-section col-md-4">
 						<span class="repartition-section-header">Calcul AIDE Pro</span>
 
 						<div class="col-sm-6 col-md-12 input_holder">
@@ -64,6 +64,24 @@
 							<div class="header-group">
 								<span class="header_label"><label for="ResultatCumuleProP_aide"> Précarité</label></span>
 								<span class="header_output" id="resultatCumuleProPAide_OP0">10 kWhc</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="repartition-section col-md-4">
+						<span class="repartition-section-header">Calcul AIDE Apporteur affaire</span>
+
+						<div class="col-sm-6 col-md-12 input_holder">
+							<div class="header-group">
+								<span class="header_label"><label for="ResultatCumuleAAHP_aide"> Hors précarité</label></span>
+								<span class="header_output" id="resultatCumuleAAHPAide_OP0">10 kWhc</span>
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-12 input_holder">
+							<div class="header-group">
+								<span class="header_label"><label for="ResultatCumuleAAP_aide"> Précarité</label></span>
+								<span class="header_output" id="resultatCumuleAAPAide_OP0">10 kWhc</span>
 							</div>
 						</div>
 					</div>
