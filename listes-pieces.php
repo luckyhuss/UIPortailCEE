3c180b5e3d32d653832cfb824885ea1703d9f257
Noodanam Chedembrun <nchedembrun@astek.mu>
2019-01-22 11:45:36 +0400

    Correction listes pièces

--- listes-pieces.php
+++ listes-pieces.php
@@ -4,188 +4,104 @@
 
             <div class="header-group col-md-12" id="sectionDocuments">
 
-                <fieldset class="fieldset-margin-bottom" id="docEngagementFieldset">
-                    <legend>Document d'engagement</legend>
-
                     <div class="col-md-12 input_holder">
-                        <span class="header_label"><label for="naturePieces">Nature</label></span> 
-                        <span class="header_output">
-                            <select id="selectDeptTravaux" name="selectDeptTravaux" class="form-control">
-                                <option value="0"></option>
-                                <option value="1">Bon de commande</option>
-                                <option value="2">Devis</option>
-                                <option value="3">#3</option>
-                            </select>
-                        </span>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <form action="upload.php" method="post" enctype="multipart/form-data">
-                        <span class="header_label"><label for="StatutEngagement">Document</label></span>
-                        <span class="header_output"><input type="file" name="fileToUpload" id="fileToUpload"></span>
-                        </form>
+                        <div class="header-group">
+                            <span class="header_label"><label for="typeOS">Opération standardisée</label></span> 
+                            <span class="header_output">
+                                <select id="selectTypeOS" name="selectTypeOS" class="form-control">
+                                    <option value="0"></option>
+                                    <option value="1">IND-UT-121</option>
+                                    <option value="2">IND-UT-122</option>
+                                    <option value="3">IND-UT-123</option>
+                                </select>
+                            </span>
+                        </div>
                     </div>
 
                     <div class="col-md-12 input_holder">
                         <div class="header-group">
-                            <span class="header_label"><label for="StatutEngagement">Statut</label></span>
-                            <span class="header_output"><input type="textarea"
-                                class="form-control" id="statutEngagement" placeholder=""></span>
+                            <span class="header_label"><label for="naturePieces">Type document</label></span> 
+                            <span class="header_output">
+                                <select id="selectTypeDoc" name="selectTypeDoc" class="form-control">
+                                    <option value="0"></option>
+                                    <option value="1">Engagement</option>
+                                    <option value="2">Achèvement</option>
+                                    <option value="3">Complémentaire</option>
+                                    <option value="4">Autre</option>
+                                </select>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 input_holder">
+                        <div class="header-group">
+                            <span class="header_label"><label for="naturePieces">Nature de la pièce</label></span> 
+                            <span class="header_output">
+                                <select id="selectNature" name="selectNature" class="form-control">
+                                    <option value="0"></option>
+                                    <option value="1">Bon de commande</option>
+                                    <option value="2">Devis</option>
+                                    <option value="3">Acte d’engagement </option>
+                                </select>
+                            </span>
                         </div>
                     </div>
 
                     <div class="col-md-12 input_holder">
                         <div class="header-group">
                             <span class="header_label"><label for="listeCritères">Critères d'éligibilité</label></span>
-                            <span class="header_output"><input type="text"
-                                class="form-control" id="critèresEngagement" placeholder=""></span>
+                            <span class="header_output"><a href="javascript:void(0)" onclick=""><i class="glyphicon glyphicon-th-list" data-toggle="modal" data-target="#listeCriteresDocModal"></i></a></span>
                         </div>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="CommentaireEngagment">Commentaire</label></span>
-                            <span class="header_output"><input type="text"
-                                class="form-control" id="comEngagement" placeholder=""></span>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 submit_button_holder">
-                        <a href="javascript:void(0)" id="sendDocEngagement" class="btn btn-primary submit_button" role="button">Envoyer</a>
-                    </div>
-
-                        <div class="col-md-12 data_table_holder">
-                            <table id="docEngagementTable"
-                                class="table table-striped table-bordered nowrap"
-                                style="width: 100%">
-                                <thead>
-                                    <tr>
-                                        <th>Nom document</th>
-                                        <th>Nature</th>
-                                        <th>Critères d'éligibilité</th>
-                                        <th>Statut</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Document 1</td>
-                                        <td>Devis</td>
-                                        <td>Critères 1</td>
-                                        <td>Envoyé</td>
-                                        <td class="iconCol"><a href="javascript:void(0)"
-                                            onclick=""><i
-                                                class="glyphicon glyphicon-eye-open" data-toggle="collapse"
-                                                data-target="#Modal1"></i></a></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
-                    </div>
-
-                </fieldset>
-
-                <fieldset class="fieldset-margin-bottom" id="docAchevementFieldset">
-                    <legend>Document d'achèvement</legend>
-
-                    <div class="col-md-12 input_holder">
-                        <span class="header_label"><label for="naturePieces">Nature</label></span> 
-                        <span class="header_output">
-                            <select id="selectDeptTravaux" name="selectDeptTravaux" class="form-control">
-                                <option value="0"></option>
-                                <option value="1">Bon de commande</option>
-                                <option value="2">Devis</option>
-                                <option value="3">#3</option>
-                            </select>
-                        </span>
                     </div>
 
                     <div class="col-md-12 input_holder">
                         <form action="upload.php" method="post" enctype="multipart/form-data">
-                        <span class="header_label"><label for="StatutEngagement">Document</label></span>
+                        <span class="header_label"><label for="NomDoc">Document</label></span>
                         <span class="header_output"><input type="file" name="fileToUpload" id="fileToUpload"></span>
                         </form>
                     </div>
 
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="StatutEngagement">Statut</label></span>
-                            <span class="header_output"><input type="textarea"
-                                class="form-control" id="statutEngagement" placeholder=""></span>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="listeCritères">Critères d'éligibilité</label></span>
-                            <span class="header_output"><input type="text"
-                                class="form-control" id="critèresEngagement" placeholder=""></span>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="CommentaireEngagment">Commentaire</label></span>
-                            <span class="header_output"><input type="text"
-                                class="form-control" id="comEngagement" placeholder=""></span>
-                        </div>
-                    </div>
-
                     <div class="col-md-12 submit_button_holder">
-                        <a href="javascript:void(0)" id="sendDocAchevement" class="btn btn-primary submit_button" role="button">Envoyer</a>
+                        <a href="javascript:void(0)" id="sendDoc" class="btn btn-primary submit_button" role="button">Envoyer</a>
                     </div>
 
-                        <div class="col-md-12 data_table_holder">
-                            <table id="docAchevementTable"
-                                class="table table-striped table-bordered nowrap"
-                                style="width: 100%">
-                                <thead>
-                                    <tr>
-                                        <th>Nom document</th>
-                                        <th>Nature</th>
-                                        <th>Critères d'éligibilité</th>
-                                        <th>Statut</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Document 1</td>
-                                        <td>Devis</td>
-                                        <td>Raison 1</td>
-                                        <td>Statut 1</td>
-                                        <td class="iconCol"><a href="javascript:void(0)"
-                                            onclick=""><i
-                                                class="glyphicon glyphicon-eye-open" data-toggle="collapse"
-                                                data-target=""></i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Document 2</td>
-                                        <td>Devis</td>
-                                        <td>Raison 2</td>
-                                        <td>Statut 2</td>
-                                        <td class="iconCol"><a href="javascript:void(0)"
-                                            onclick=""><i
-                                                class="glyphicon glyphicon-eye-open" data-toggle="collapse"
-                                                data-target=""></i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Document 3</td>
-                                        <td>Devis</td>
-                                        <td>Raison 3</td>
-                                        <td>Statut 3</td>
-                                        <td class="iconCol"><a href="javascript:void(0)"
-                                            onclick=""><i
-                                                class="glyphicon glyphicon-eye-open" data-toggle="collapse"
-                                                data-target=""></i></a></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
+                    <div class="col-md-12 data_table_holder">
+                        <table id="listePiecesTable"
+                            class="table table-striped table-bordered nowrap"
+                            style="width: 100%">
+                            <thead>
+                                <tr>
+                                    <th>Opération standardisée</th>
+                                    <th>Type document</th>
+                                    <th>Nom document</th>
+                                    <th>Nature de la pièce</th>
+                                    <th>Statut</th>
+                                    <th>Commentaires</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>IND-UT-122</td>
+                                    <td>Engagement</td>
+                                    <td>Document 1</td>
+                                    <td>Devis</td>
+                                    <td>Refusé</td>
+                                    <td class="iconCol"><a href="javascript:void(0)"
+                                        onclick=""><i
+                                            class="glyphicon glyphicon-eye-open" data-toggle="modal"
+                                            data-target="#commentaireModal"></i></a></td>
+                                </tr>
+                                <tr>
+                                    <td>IND-UT-121</td>
+                                    <td>Engagement</td>
+                                    <td>Document 1</td>
+                                    <td>Bon de commande</td>
+                                    <td>Envoyé</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
-
-                </fieldset>
-
             </div>
 
         </div>
@@ -193,18 +109,20 @@
 </div>
 
 
-<div class="modal fade popin-donnee" id="Modal1" tabindex="-1"
+<div class="modal fade popin-donnee" id="listeCriteresDocModal" tabindex="-1"
 	role="dialog" aria-labelledby="myModalLabel">
 	<div class="modal-dialog liste_critere" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<button type="button" class="btn btn-default" data-dismiss="modal">x</button>
-				<h4 class="modal-title" id="myModalLabel">Commentaire</h4>
+				<button type="button" class="btn btn-default custom-close">x</button>
+				<h4 class="modal-title" id="myModalLabel">Critères d'éligibilité</h4>
 			</div>
 
 			<div class="modal-body">
 				<ul>
-					<li>val1</li>
+                    <li>Zone climatique : H1</li>
+                    <li>Superficie d'isolant (m²) : 100</li>
+                    <li>Energie de chauffage après travaux : 100</li>
 				</ul>
 			</div>
 
@@ -214,3 +132,35 @@
 		</div>
 	</div>
 </div>
+
+<div class="modal fade popin-donnee" id="commentaireModal" tabindex="-1"
+	role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog liste_critere" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="btn btn-default custom-close">x</button>
+				<h4 class="modal-title" id="myModalLabel">Commentaires</h4>
+			</div>
+
+			<div class="modal-body">
+				<ul>
+                    <li>Refusé</li>
+                    <li>Voir les critères d'éligibilité</li>
+				</ul>
+			</div>
+
+			<div class="modal-footer">
+				<!-- <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button> -->
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+        $(function () {
+    $(".custom-close").on('click', function() {
+        $('#commentaireModal').modal('hide');
+        $('#listeCriteresDocModal').modal('hide');
+    });
+});
+</script>
