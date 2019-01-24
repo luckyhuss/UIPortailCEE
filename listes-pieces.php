c4892bb7b2a0795fe42978b9a511532471be62d6
Noodanam Chedembrun <nchedembrun@astek.mu>
2019-01-24 09:41:17 +0400

    liste pieces in a pop up

--- listes-pieces.php
+++ listes-pieces.php
@@ -2,69 +2,12 @@
     <fieldset class="fieldset-margin-bottom"><legend>Listes pièces</legend>
         <div class="fieldset-container">
 
+            <div class="col-md-12 submit_button_holder">
+                <a href="javascript:void(0)" id="ajouterUnDoc" class="btn btn-primary submit_button" role="button" data-toggle="modal"
+                                    data-target="#uploadFileModal">Ajouter un document</a>
+            </div>
+
             <div class="header-group col-md-12" id="sectionDocuments">
-
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="typeOS">Opération standardisée</label></span> 
-                            <span class="header_output">
-                                <select id="selectTypeOS" name="selectTypeOS" class="form-control">
-                                    <option value="0"></option>
-                                    <option value="1">IND-UT-121</option>
-                                    <option value="2">IND-UT-122</option>
-                                    <option value="3">IND-UT-123</option>
-                                </select>
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="naturePieces">Type document</label></span> 
-                            <span class="header_output">
-                                <select id="selectTypeDoc" name="selectTypeDoc" class="form-control">
-                                    <option value="0"></option>
-                                    <option value="1">Engagement</option>
-                                    <option value="2">Achèvement</option>
-                                    <option value="3">Complémentaire</option>
-                                    <option value="4">Autre</option>
-                                </select>
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="naturePieces">Nature de la pièce</label></span> 
-                            <span class="header_output">
-                                <select id="selectNature" name="selectNature" class="form-control">
-                                    <option value="0"></option>
-                                    <option value="1">Bon de commande</option>
-                                    <option value="2">Devis</option>
-                                    <option value="3">Acte d’engagement </option>
-                                </select>
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <div class="header-group">
-                            <span class="header_label"><label for="listeCritères">Critères d'éligibilité</label></span>
-                            <span class="header_output"><a href="javascript:void(0)" onclick=""><i class="glyphicon glyphicon-th-list" data-toggle="modal" data-target="#listeCriteresDocModal"></i></a></span>
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 input_holder">
-                        <form action="upload.php" method="post" enctype="multipart/form-data">
-                        <span class="header_label"><label for="NomDoc">Document</label></span>
-                        <span class="header_output"><input type="file" name="fileToUpload" id="fileToUpload"></span>
-                        </form>
-                    </div>
-
-                    <div class="col-md-12 submit_button_holder">
-                        <a href="javascript:void(0)" id="sendDoc" class="btn btn-primary submit_button" role="button">Envoyer</a>
-                    </div>
-
                     <div class="col-md-12 data_table_holder">
                         <table id="listePiecesTable"
                             class="table table-striped table-bordered nowrap"
@@ -156,11 +99,137 @@
 	</div>
 </div>
 
+<div class="modal fade popin-donnee" id="uploadFileModal" tabindex="-1"
+	role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog liste_critere" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="btn btn-default" data-dismiss="modal">x</button>
+				<h4 class="modal-title" id="myModalLabel">Ajouter un document</h4>
+			</div>
+
+			<div class="modal-body">
+                <div class="col-md-12 input_holder">
+                    <div class="header-group">
+                        <span class="header_label"><label for="typeOS">Opération standardisée</label></span> 
+                        <span class="header_output">
+                            <select id="selectTypeOS" name="selectTypeOS" class="form-control">
+                                <option value="0"></option>
+                                <option value="1">IND-UT-121</option>
+                                <option value="2">IND-UT-122</option>
+                                <option value="3">IND-UT-123</option>
+                            </select>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="col-md-12 input_holder">
+                    <div class="header-group">
+                        <span class="header_label"><label for="naturePieces">Type document</label></span> 
+                        <span class="header_output">
+                            <select id="selectTypeDoc" name="selectTypeDoc" class="form-control">
+                                <option value="0"></option>
+                                <option value="1">Engagement</option>
+                                <option value="2">Achèvement</option>
+                                <option value="3">Complémentaire</option>
+                                <option value="4">Autre</option>
+                            </select>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="col-md-12 input_holder">
+                    <div class="header-group">
+                        <span class="header_label"><label for="naturePieces">Nature de la pièce</label></span> 
+                        <span class="header_output">
+                            <select id="selectNature" name="selectNature" class="form-control">
+                                <option value="0"></option>
+                                <option value="1">Bon de commande</option>
+                                <option value="2">Devis</option>
+                                <option value="3">Acte d’engagement </option>
+                            </select>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="col-md-12 input_holder">
+                    <div class="header-group">
+                        <span class="header_label"><label for="listeCritères">Critères d'éligibilité</label></span>
+                        <span class="header_output"><a href="javascript:void(0)" onclick=""><i class="glyphicon glyphicon-th-list" data-toggle="modal" data-target="#listeCriteresDocModal"></i></a></span>
+                    </div>
+                </div>
+
+                    <div class="col-md-12 input_holder">
+                        <div class="header-group">
+                            <span class="header_label"><label for="choisirDoc">Document</label></span>
+                            <span class="header_output">
+                                <input type=file style="display: none" id=choose>
+                                <input type=button onClick=getFile.simulate() value="Choisir un fichier">
+                                <label id=selected>Pas de fichier</label>
+                            </span>
+                        </div>
+                    </div>
+			</div>
+
+			<div class="modal-footer">
+                <div class="col-md-12 submit_button_holder">
+                    <a href="javascript:void(0)" id="sendDoc" class="btn btn-primary submit_button" role="button">Envoyer</a>
+                </div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 <script>
-        $(function () {
-    $(".custom-close").on('click', function() {
-        $('#commentaireModal').modal('hide');
-        $('#listeCriteresDocModal').modal('hide');
-    });
-});
+
+    const selectFile = function() {
+	
+	let regex = /[^\\]+$/
+	
+	this.choose,
+	this.selected
+	
+	this.msg = str => {
+		let prefix = '[selectFile.js]\n\nError: '
+		return alert(prefix+str)
+	}
+		
+	this.check = () => {
+		if (this.choose && this.selected != null) {
+			let choose = document.getElementById(this.choose),
+				selected = document.getElementById(this.selected)
+			choose.addEventListener('change',() => {
+				if (choose.value != '') { 
+					selected.innerHTML = choose.value.match(regex)
+				}
+			})
+		} else {
+			this.msg('Targets not set.')
+		}
+	}
+	
+	selectFile.prototype.targets = (trigger, filetext) => {
+		this.choose = trigger
+		this.selected = filetext
+	}
+	
+	selectFile.prototype.simulate = () => {
+		if (this.choose != null) {
+			let choose = document.getElementById(this.choose)
+			if (typeof choose != 'undefined') {
+				choose.click()
+				this.check()
+			} else {
+				this.msg('Could not find element '+this.choose)
+			}
+		} else {
+			this.msg('Targets not set.')
+		}
+	}	
+	
+};
+
+var getFile = new selectFile;
+getFile.targets('choose','selected');
 </script>
