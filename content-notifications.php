1a170b261c9163c8df6596cf46efe82ea4118d90
Rodney Jean Pierre <rjeanpierre@astek.mu>
2019-01-30 16:08:35 +0400

    alerte/notification

---
+++ content-notifications.php
@@ -0,0 +1,120 @@
+<div class="panel-group">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<span class="panel_header_element"><a data-toggle="collapse"
+					href="#listeRefusPieces">Refus Pièces Not</a></span> <span
+					class="panel_header_element"><a data-toggle="collapse"
+					href="#listeRefusPieces"><i class="glyphicon glyphicon-list-alt"></i></a></span>
+			</h4>
+		</div>
+
+		<div id="listeRefusPieces" class="panel-collapse collapse in">
+			<div class="panel-body">
+
+				<div class="row">
+					<div class="col-md-12 data_table_holder">
+						<table id="listRefusPieces"
+							class="table table-striped table-bordered nowrap listDataTable"
+							style="width: 100%">
+							<thead>
+								<tr>
+									<th>Numéro Dossier</th>
+									<th>Statut de la pièce</th>
+									<th>Nature de la pièce</th>
+
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+							
+								<?php
+                                for ($n = 0; $n <= 50; $n ++) {
+                                    ?>
+                        
+                        		<tr>
+									<td>This is a test <?=$n?></td>
+									<td>Rejeté</td>
+									<td>This is a test</td>
+									<td class="iconCol"><a
+										href="/uiportailcee?pageLoaded=Dossiers&id=<?=$n ?>"><i
+											class="glyphicon glyphicon-eye-open"></i></a></td>
+								</tr>
+                        
+                        		<?php
+                                }
+                                ?>
+								
+							</tbody>
+						</table>
+
+					</div>
+				</div>
+
+				<!-- Closing collapsible wrapper -->
+			</div>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="panel-group">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<span class="panel_header_element"><a data-toggle="collapse"
+					href="#listeRefusDossiers">Refus Dossiers</a></span> <span
+					class="panel_header_element"><a data-toggle="collapse"
+					href="#listeRefusDossiers"><i class="glyphicon glyphicon-list-alt"></i></a></span>
+			</h4>
+		</div>
+
+		<div id="listeRefusDossiers" class="panel-collapse collapse in">
+			<div class="panel-body">
+
+				<div class="row">
+					<div class="col-md-12 data_table_holder">
+						<table id="listRefusDossiers"
+							class="table table-striped table-bordered nowrap listDataTable"
+							style="width: 100%">
+							<thead>
+								<tr>
+									<th>Numéro Dossier</th>
+									<th>Statut de la pièce</th>
+									<th>Raison d'inéligibilité</th>
+
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+							
+								<?php
+                                for ($n = 0; $n <= 50; $n ++) {
+                                    ?>
+                        
+                        		<tr>
+									<td>4587962 <?=$n?></td>
+									<td>En attente</td>
+									<td>This is a test</td>
+									<td class="iconCol"><a
+										href="/uiportailcee?pageLoaded=Dossiers&id=<?=$n ?>"><i
+											class="glyphicon glyphicon-eye-open"></i></a></td>
+								</tr>
+                        
+                        		<?php
+                                }
+                                ?>
+								
+							</tbody>
+						</table>
+
+					</div>
+				</div>
+
+				<!-- Closing collapsible wrapper -->
+			</div>
+		</div>
+
+	</div>
+</div>
---
+++ demande-habilitation.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>TOTAL -</title>
+<link rel="icon" type="image/png" href="assets/img/total-icon.png">
+<link href="assets/css/main.css" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link href="assets/css/simple-layout.css" rel="stylesheet">
+
+
+<!-- Bootstrap -->
+<link href="node_modules/bootstrap/dist/css/bootstrap.min.css"
+	rel="stylesheet">
+</head>
+	<body>
+
+    	<header>
+    
+    		<nav class="navbar navbar-default navbar-fixed-top">
+    
+    			<div class="page_banner">
+		<div class="page_total-logo">
+			<img alt="" src="assets/img/total-logo.png">
+		</div>
+	</div>
+    		</nav>
+    	</header>
+    
+    	<section class="page_area">
+    		<div class="container-fluid">
+    			<div class="row flex-container">
+    			
+    				
+    			
+    			</div>
+    		</div>
+    	</section>
+    	
+    	<?php include ('footer.php'); ?>
+
+	</body>
+</html>
