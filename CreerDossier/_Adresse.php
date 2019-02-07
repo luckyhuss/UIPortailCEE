aa2e536275a731cf78e19ba3fbc9b355b24c4e4e
Noodanam CHEDEMBRUN <nchedembrun@astek.mu>
2019-02-07 15:35:09 +0400

    Add adresse des travaux

---
+++ CreerDossier/_Adresse.php
@@ -0,0 +1,23 @@
+<div class="panel-group" id="adressePanel">
+	<div class="panel panel-default">
+		<div class="panel-body">
+
+        <fieldset class="fieldset-margin-bottom choisirAdressePanel">
+            <legend>Adresse</legend>
+
+            <div class="fieldset-container" id="repartitionAdresseFieldset">
+
+                <div class="input_holder">
+                    <div class="header-group">
+                        <span class="form_label"><label for="AddrDesTravaux">Adresse des travaux</label></span> <span
+                            class="header_output"><input type="text"
+                            class="form-control" id="addrDesTravaux" placeholder=""></span>
+                    </div>
+                </div>
+                
+            </div>
+        </fieldset>
+
+		</div>
+	</div>
+</div>
