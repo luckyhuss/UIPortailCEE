39bf61506680e553a214275b793d97670fa7ce26
Noodanam CHEDEMBRUN <nchedembrun@astek.mu>
2019-02-08 11:40:57 +0400

    Create stepper file

--- Simulation/simuler-AS24-B2B.php
+++ Simulation/simuler-AS24-B2B.php
@@ -1,35 +1,5 @@
-<div class="stepwizardCreer">
-    <div class="stepwizardCreer-row setup-panel displayed">
 
-      <div class="stepwizardCreer-step">
-        <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
-        <p>étape 1</p>
-	  </div>
-	  
-      <div class="stepwizardCreer-step">
-        <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
-        <p>étape 2</p>
-	  </div>
-	  
-      <div class="stepwizardCreer-step">
-        <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
-        <p>étape 3</p>
-	  </div>
-
-	  <div class="stepwizardCreer-step">
-        <a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
-        <p>étape 4</p>
-      </div>
-      
-      <div class="stepwizardCreer-step">
-        <a href="#step-5" type="button" class="btn btn-default btn-circle" disabled="disabled">5</a>
-        <p>étape 5</p>
-	  </div>
-	  
-	</div>
-	
-</div>
-
+<?php include '_Stepper.php'; ?>
 
 <form role="form" action="" method="post">
     <div class="setup-content" id="step-1">
